c0d0030caeb8d544edd0ad5ded0334dd81ff3f15
NumAniCloud <num.ani.cloud@gmail.com>
2023-10-03 15:10:58 +0900

    動作するように

--- MultipleProjectTest.Godot/ContextToAttach.cs
+++ MultipleProjectTest.Godot/ContextToAttach.cs
@@ -5,7 +5,7 @@ using Godot;
 
 namespace MultipleProjectTest;
 
-public partial class ContextToAttach : Node
+public partial class ContextToAttach : Sprite2D
 {
     [Export] public Sprite2D Sprite { get; private set; }
     
